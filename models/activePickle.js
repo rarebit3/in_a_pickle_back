969dd88354b427790f6fe818a87751662c999c3b
rarebit3 <johntrevorbond@gmail.com>
2022-05-09 13:37:07 -0400

    user and activePickle models

--- models/activePickle.js
+++ models/activePickle.js
@@ -3,7 +3,12 @@ const Schema = mongoose.Schema;
 
 const Pickle = new Schema(
   {
-
+    uesr: { type: String, required: true },
+    recipe: { type: String, required: true },
+    startDate: { type: String, required: true },
+    tooOld: { type: String, required: true },
+    checkInterval: { type: String, required: true },
+    reminders: { type: Boolean, required: true },
   },
   { timestamps: true }
 );
