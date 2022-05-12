9e853e4a1db0ed4c3061a6b75a5aed86552b9125
rarebit3 <johntrevorbond@gmail.com>
2022-05-12 09:47:40 -0400

    projects page draft

---
+++ client/src/components/Projects.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import "./Projects.css";
+
+const Projects = () => {
+    let navigate = useNavigate()
+
+    //define the states
+    const [projects, setProjects] = useState([])
+
+    //function that makes an axios call to set the state
+    const getProjects = async () => {
+        const projectList = await axios.get("/projects")
+        setProjects(projectList.data.projects)
+    }
+
+    //make the axios call on pageload
+    useEffect(() => {
+        getProjects()
+    }, [])
+
+    //Your on-clicks and other event listeners will go here
+
+    if (!projects) {
+        return <h1>Loading your active pickling projects!</h1>
+    } else {
+        return (
+            <div>{/*Map your projects, ya fool! */}</div>
+        )
+    
+    }
+
+}
+
+export default Projects
